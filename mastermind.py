34b2995ab2972a04cfed4f7f8c14aef7d86e58c0
Matthias Brettschneider <frosch03@gmail.com>
2014-11-28 04:16:58 +0100

    added a solutions-class

--- mastermind.py
+++ mastermind.py
@@ -33,21 +33,36 @@ class Board:
         return ((cap.count(True), c.count(True)))
 
 
-def genTree(_a, depth = 4):
-    def _genTree(_a, _depth):
-        if _depth > 0:
-            return([(_a.alphabeth[i], _genTree(_a, (_depth-1))) for i in range(len(_a.alphabeth))])
-        else:
-            return([])
+class Solutions:
+    def __init__(self, _a, depth = 4):
+        self.genTree(_a, depth)
 
-    return (("", _genTree(_a, depth)))
+    def genTree(self, _a, depth = 4):
+        def _genTree(_a, _depth):
+            if _depth > 0:
+                return([(_a.alphabeth[i], _genTree(_a, (_depth-1))) for i in range(len(_a.alphabeth))])
+            else:
+                return([])
+        self.__tree = (("", _genTree(_a, depth)))
+        
+    def solutions(self):
+        def leafs(_tree):
+            value    = _tree[0]
+            subtrees = _tree[1]
+        
+            if len(subtrees) == 0:
+                return ([str(value)])
+        
+            return ([(str(value) + leaf) for subtree in subtrees for leaf in leafs(subtree)])
+        return(leafs(self.__tree))
     
-def leafs(_tree):
-    value    = _tree[0]
-    subtrees = _tree[1]
-
-    if len(subtrees) == 0:
-        return ([str(value)])
-
-    return ([(str(value) + leaf) for subtree in subtrees for leaf in leafs(subtree)])
- 
+    def delete(self, _char, pos = None):
+        def inner_delete(_tree, _char, pos = None):
+            value    = _tree[0]
+            subtrees = _tree[1]
+        
+            if None == pos:
+                return (value, [inner_delete(subtree, _char, None) for subtree in subtrees if subtree[0] != _char]) 
+            else:
+                return (value, [deleteAt(subtree, _char, pos-1) for subtree in subtrees if subtree[0] != _char or (pos-1) != 0])
+        self.__tree = inner_delete(self.__tree, _char, pos)
