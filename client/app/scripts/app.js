feb3cb491ff77e519fbc66da2ccb30b95c0fde2a
Matthias Brettschneider <frosch03@gmail.com>
2014-11-30 12:30:58 +0100

    fixed send-data bug

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -9,55 +9,51 @@
  * Main module of the application.
  */
 angular
-  .module('mastermindApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .constant('API_END_POINT', 'http://10.0.101.42:8080')
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  })
-  .factory("Board", [ '$resource', 'API_END_POINT', function ($resource, API_END_POINT) {
-    return $resource(API_END_POINT + '/:action', {
-    }, {
-        get: {
-            method: 'GET',
-            params: {
-                'action': 'get'
-            }
-        },
-        move: {
-            method: 'POST',
-            params: {
-                'action': 'move'
-            }
-        },
-        create: {
-            method: 'POST',
-            params: {
-                'action': 'move'
-            }
-        }
+    .module('mastermindApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch'
+    ])
+    .constant('API_END_POINT', 'http://10.0.101.42:8080')
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    })
+    .factory("Board", [ '$resource', 'API_END_POINT', function ($resource, API_END_POINT) {
+        return $resource(API_END_POINT + '/:action', { 
+        }, {
+          'get': {
+              method: 'GET',
+              params: {
+                  action: 'get'
+              }
+          },
+          'save': {
+              method: 'POST',
+              params: {
+                  action: 'move'
+              }
+          },
+          'delete': {
+              method: 'GET',
+              params: {
+                  'action': 'new'
+              }
+          }
 
-    });
-  } 
+        });
+    } 
 ]);
-
-  // .factory("Board", function($resource) {
-  //   return $resource("http://10.0.101.42:8080/board");
-  // });
--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -8,44 +8,44 @@
  * Controller of the mastermindApp
  */
 angular.module('mastermindApp')
-  .controller('MainCtrl', function ($scope, $http, Board) {
+    .controller('MainCtrl', function ($scope, $http, Board) {
 
-	Board.get(function(success) {
-		$scope.game = success;
-		console.log(success);
-		$scope.actualBoard = { 
-			move: [0,0,0,0]
-		};
-	}, function(error) {
-		$scope.game = {
-			state: "running",
-			
-		}
-	});
+	      Board.get(function(success) {
+		        $scope.game = success;
+		        console.log(success);
+		        $scope.actualBoard = { 
+			          move: [0,0,0,0]
+		        };
+	      }, function(error) {
+		        $scope.game = {
+			          state: "running",
+			          
+		        }
+	      });
 
-	$scope.setActualColor = function(cId) {
-		$scope.actualColor = cId;
-	}
-	$scope.setColor = function(ndx) {
-		$scope.actualBoard.move[ndx] = $scope.actualColor;
-	}
+	      $scope.setActualColor = function(cId) {
+		        $scope.actualColor = cId;
+	      }
+	      $scope.setColor = function(ndx) {
+		        $scope.actualBoard.move[ndx] = $scope.actualColor;
+	      }
 
-	$scope.send = function() {
-		$scope.game.board.push($scope.actualBoard);
-		$scope.actualBoard = { 
-			move: [0,0,0,0]
-		};
-		return;
-		Board.move({ request: "1234" 
-		}, function (error) {
-            console.log("blub");	
-        }, function (success) {
-            console.log(success);	
-        });
-	//	Board.move({ request: "1234" }, function(success) {
-	//		console.log(success);
-	//	});
-
-	}
-	
-  });
+	      $scope.send = function() {
+		        // $scope.game.board.push($scope.actualBoard);
+		        // $scope.actualBoard = { 
+			      //     move: [0,0,0,0]
+		        // };
+		        // return;
+		        Board.save({request: "1234"}, function (success) {
+                           console.log("1");
+                           console.log(success);
+                       }, function (error) {
+                           console.log("2")
+                           console.log(error);	
+                       });
+	          //	Board.move({ request: "1234" }, function(success) {
+	          //		console.log(success);
+	          //	});
+	      }
+	      
+    });
