30f753ac8732a16a411c7aa7ffdf1532d61444c2
Matthias Brettschneider <frosch03@gmail.com>
2014-12-03 18:10:20 +0100

    added won-detection

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -1,29 +1,28 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name mastermindApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the mastermindApp
- */
 angular.module('mastermindApp')
     .controller('MainCtrl', function ($scope, $http, Board) {
         $scope.colorMap = ['white', 'red', 'brown', 'indigo', 'yellow', 'green', 'purple'];
         $scope.index = 0;
-        
-	      Board.get(function(success) {
-		        $scope.game = success;
-		        console.log(success);
+        $scope.ended = false;
+        $scope.won   = false;
+
+        $scope.doMove = function (game) {
+            $scope.index = 0;
+		        $scope.game = game;
+            // if game ended :
+            if (game.ttl == 0 || game.state != "running") {
+                $scope.ended = true
+            };
+            // if game ended by winning :
+            if (game.ttl != 0 && game.state == "end") {
+                $scope.won = true
+            };
 		        $scope.actualBoard = { 
 			          move: [0,0,0,0]
 		        };
-	      }, function(error) {
-		        $scope.game = {
-			          state: "running",
-			          
-		        }
-	      });
+        }
+
 
 	      $scope.setActualColor = function(cId) {
 		        $scope.actualColor = $scope.colorMap[cId];
@@ -35,12 +34,20 @@ angular.module('mastermindApp')
             if($scope.index == 3){$scope.index = 0;} else {$scope.index += 1;};
 	      }
 
+        
+	      Board.get(function(success) {
+            $scope.doMove(success);
+		        console.log(success);
+	      }, function(error) {
+		        $scope.game = {
+			          state: "running",
+			          
+		        }
+	      });
+
 	      $scope.send = function() {
 		        Board.save({request: $scope.actualBoard.move}, function (success) {
-                $scope.game = success;
-		            $scope.actualBoard = { 
-			              move: [0,0,0,0]
-		            };
+                $scope.doMove(success);
             }, function (error) {
                 console.log("error: "+error);	
             });
@@ -50,5 +57,7 @@ angular.module('mastermindApp')
 		        $scope.game = {
 			          state: "running",
 		        };
+            $scope.ended = false;
+            $scope.won   = false;
         }
     });
